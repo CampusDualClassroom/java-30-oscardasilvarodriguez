049e0b9ed2be68d403601ec49e6681fe99e340a8
oscardasilvarodriguez <grelodj@hotmail.com>
2025-02-13 14:07:58 +0100

    ejercicio-java-30 re-corregido

--- src/main/java/com/campusdual/classroom/Exercise30.java
+++ src/main/java/com/campusdual/classroom/Exercise30.java
@@ -14,7 +14,7 @@ public class Exercise30 {
         try {
             System.out.println(divisionWithCustomException(10, 0));
         } catch (DivisionByZeroException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }finally{
         System.out.println("Programa terminado.");
         }
